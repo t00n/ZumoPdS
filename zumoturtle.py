5ea03c5a1e424a93d713c39fa037102061479cf1
iTitou <moiandme@gmail.com>
2015-03-08 23:51:58 +0100

    [fix] Arduino Yun Bridge communication

--- zumoturtle.py
+++ zumoturtle.py
@@ -1,37 +1,34 @@
-import socket
+import traceback
 
 try:
-    HOST = 'localhost'    # The remote host
-    PORT = 6571             # The same port as used by the server
+    import socket
+    HOST = 'localhost'
+    PORT = 6571
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
 
+    def send_32u4(fmt, *args):
+        sock.sendall((fmt+"\n") % args)
+        while sock.recv(1) != "\n":
+            pass
+
     def forward(length):
-        print "  forward", length
-        sock.sendall("fw %d" % length)
-        sock.read(1024)
+        send_32u4("fw %d\n", length)
 
     def backward(length):
-        print " backward", length
-        sock.sendall("bw %d" % length)
-        sock.read(1024)
+        send_32u4("bw %d\n", length)
 
     def turnLeft(angle):
-        print " turnLeft", angle
-        sock.sendall("tl %d" % angle)
-        sock.read(1024)
+        send_32u4("tl %d\n", angle)
 
     def turnRight(angle):
-        print "turnRight", angle
-        sock.sendall("tr %d" % angle)
-        sock.read(1024)
+        send_32u4("tr %d\n", angle)
 
     def setSpeed(speed):
-        print " setSpeed", speed
-        sock.sendall("ss %d" % speed)
-        sock.read(1024)
+        send_32u4("ss %d\n", speed)
 
 except:
+    traceback.print_exc()
     print "\033[33;1m[WARNING]\033[0m No Arduino YUN Bridge..."
 
     def forward(length):
