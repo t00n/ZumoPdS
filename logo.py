39b9a2df69e8b28926eb1c09dfd92e923c255863
iTitou <moiandme@gmail.com>
2015-03-09 09:55:52 +0100

    [enh] Better REPL and error management

--- logo.py
+++ logo.py
@@ -14,6 +14,19 @@ import math
 
 # http://slps.github.io/zoo/logo/sdf.html
 
+class ParseError(Exception):
+    pass
+
+class UnterminatedExpression(ParseError):
+    pass
+
+class ProgramError(Exception):
+    pass
+
+class UnknowIdentifier(ProgramError):
+    pass
+
+
 class Env(object):
     """An environment object, referencing its outer environment"""
 
@@ -26,7 +39,7 @@ class Env(object):
             return self.content[key]
         elif self.parent is not None:
             return self.parent[key]
-        raise KeyError(key)
+        raise UnknowIdentifier(key)
 
     def __setitem__(self, key, val):
         self.content[key] = val
@@ -72,12 +85,12 @@ class Primitive(Callable):
     def call(self, env, args):
         return self.func(*args)
 
-class ParseError(Exception):
-    pass
-
 def const(value):
     """Return a lambda(env) returning a constant value"""
     return lambda env: value
+
+def logo_list(expressions):
+    return lambda env: map(lambda expr: expr(env), expressions)
 
 def sequentially(functions):
     """
@@ -97,48 +110,88 @@ def repetition(times, func):
     return f
 
 class Evaluator(object):
-    def __init__(self, env=None):
-        add = lambda a,b: a+b
-        sub = lambda a,b: a-b
-        mul = lambda a,b: a*b
-        div = lambda a,b: a/b
-        math_primitives = {
-            "+": Primitive(add, 2),     "-": Primitive(sub, 2), 
-            "*": Primitive(mul, 2),     "/": Primitive(div, 2),
-            "sin": Primitive(math.sin), "cos": Primitive(math.cos),
-        }
-        self.env = Env(parent=env, **math_primitives)
+    Keywords_fr = {
+        "LOOP": "repete", 
+        "DEF_PROC": "pour",
+        "END_PROC": "fin"
+    }
 
-    def analyze_repetition(self, tokens, i):
-        times = int(tokens[i])
-        if tokens[i+1] != "[":
-            raise ParseError("Missing '['")
-        i += 2
+    Add = lambda a,b: a+b
+    Sub = lambda a,b: a-b
+    Mul = lambda a,b: a*b
+    Div = lambda a,b: a/b
+    Math_primitives = {
+        "+": Primitive(Add, 2),     "-": Primitive(Sub, 2), 
+        "*": Primitive(Mul, 2),     "/": Primitive(Div, 2),
+        "sin": Primitive(math.sin), "cos": Primitive(math.cos),
+    }
 
-        funcs = []
-        while tokens[i] != "]":
-            f, i = self.analyze(tokens, i)
-            funcs.append(f)
+    def __init__(self, env=None, keywords=Keywords_fr):
+        self.env = Env(parent=env, **self.Math_primitives)
+        self.keywords = keywords
+
+    def analyze_list(self, tokens, i):
+        expr = []
+        try:
+            while tokens[i] != "]":
+                e, i = self.analyze(tokens, i)
+                expr.append(e)
+        except IndexError:
+            raise UnterminatedExpression("Unterminated loop")
         
-        return repetition(times, sequentially(funcs)), i+1
+        return logo_list(expr), i+1
 
     def analyze_procedure(self, tokens, i):
         name = tokens[i]
         i += 1
 
-        args = []
-        while tokens[i][0] == ':':
-            args.append(tokens[i])
-            i += 1
+        try:
+            args = []
+            while tokens[i][0] == ':':
+                args.append(tokens[i])
+                i += 1
 
-        funcs = []
-        while tokens[i] != "fin":
-            f, i = self.analyze(tokens, i)
-            funcs.append(f)
+            funcs = []
+            while tokens[i] != self.keywords["END_PROC"]:
+                f, i = self.analyze(tokens, i)
+                funcs.append(f)
+        except IndexError:
+            raise UnterminatedExpression("Unterminated procedure definition")
 
         body = sequentially(funcs)
         self.env[name] = Procedure(args, body)
         return None, i+1
+
+    def analyze_string(self, tokens, i):
+        words = []
+        try:
+            while tokens[i] != '"':
+                words.append(tokens[i])
+                i += 1
+        except IndexError:
+            raise UnterminatedExpression("Unterminated String")
+        return const(" ".join(words)), i+1
+
+    def analyze_call(self, procedure, tokens, i):
+        args = []
+        try:
+            for argno in range(procedure.arity):
+                arg, i = self.analyze(tokens, i)
+                args.append(arg)
+        except IndexError:
+            raise UnterminatedExpression("Missing arguments in call")
+        return lambda env: procedure.call(env, map(lambda x: x(env), args)), i
+
+    def analyze_loop(self, tokens, i):
+        try:
+            times = int(tokens[i])
+        except IndexError:
+            raise UnterminatedExpression("Missing loop count")
+        try:
+            expr, i = self.analyze(tokens, i+1)
+        except IndexError:
+            raise UnterminatedExpression("Missing loop body")
+        return repetition(times, expr), i
 
     def analyze(self, tokens, i):
         """
@@ -152,32 +205,26 @@ class Evaluator(object):
             return const(int(tok)), i
 
         elif tok == '"':
-            words = []
-            while tokens[i] != '"':
-                words.append(tokens[i])
-                i += 1
-            i += 1
-            return const(" ".join(words)), i
+            return self.analyze_string(tokens, i)
 
-        elif tok == "repete":
-            return self.analyze_repetition(tokens, i)
+        elif tok == '[':
+            return self.analyze_list(tokens, i)
 
-        elif tok == "pour":
+        elif tok == self.keywords["LOOP"]:
+            return self.analyze_loop(tokens, i)
+
+        elif tok == self.keywords["DEF_PROC"]:
             return self.analyze_procedure(tokens, i)
 
         elif tok in self.env and isinstance(self.env[tok], Callable):
-            proc = self.env[tok]
-            args = []
-            for argno in range(proc.arity):
-                arg, i = self.analyze(tokens, i)
-                args.append(arg)
-            return lambda env: proc.call(env, map(lambda x: x(env), args)), i
-
+            return self.analyze_call(self.env[tok], tokens, i)
+            
         else:
             return Env.lookup(tok), i
 
     def eval(self, text):
-        tokens = text.replace('[', ' [ ').replace(']', ' ] ').replace('"', ' " ').split()
+        lines = filter(lambda line: not line.strip().startswith(';'), text.split('\n'))
+        tokens = ' '.join(lines).replace('[', ' [ ').replace(']', ' ] ').replace('"', ' " ').split()
         i, prog = 0, []
         while i < len(tokens):
             func, i = self.analyze(tokens, i)
@@ -185,38 +232,53 @@ class Evaluator(object):
                 prog.append(func)
         return sequentially(prog)(self.env)
 
-if __name__ == "__main__":
-    from sys import argv, stdin
-    from zumoturtle import forward, backward, turnLeft, turnRight, setSpeed
-
-    def wrap_print(text): 
-        print text
-
-    primitives_fr = {
-        "ga": Primitive(turnLeft),  "gauche": Primitive(turnLeft),
-        "dr": Primitive(turnRight), "droite": Primitive(turnRight),
-        "av": Primitive(forward),   "avance": Primitive(forward),
-        "re": Primitive(backward),  "recule": Primitive(backward),
-        "vi": Primitive(setSpeed),  "vitesse": Primitive(setSpeed),
-        "p": Primitive(wrap_print), "print": Primitive(wrap_print),
-        "q": Primitive(exit, 0),
-    }
-
-    interpreter = Evaluator(env=Env(**primitives_fr))
-
-    if len(argv) > 1:
-        for script in argv[1:]:
-            retval = interpreter.eval(open(script).read())
-            print " =>", retval
-    else:
+    def repl(self, user_input=raw_input):
+        text, prompt = "", " > "
         while True:
             try:
-                retval = interpreter.eval(raw_input(" > "))
+                text += user_input(prompt)
+                retval = self.eval(text)
+                text, prompt = "", "> "
                 print " => ", retval
+            except UnterminatedExpression:
+                text += "\n"
+                prompt = " ... "
+            except ParseError as err:
+                print "\033[31;1m[ERROR]\033[0m Syntax error in code"
+                print "%s: %s" % (err.__class__.__name__, err)
+            except ProgramError as err:
+                print "\033[31;1m[ERROR]\033[0m Error in running program"
+                print "%s: %s" % (err.__class__.__name__, err)
             except KeyboardInterrupt:
                 print
                 continue
             except EOFError:
                 print
                 break
+
+if __name__ == "__main__":
+    from sys import argv, stdin
+    from zumoturtle import forward, backward, turnLeft, turnRight, setSpeed
+    import traceback
+
+    def wrap_print(text): 
+        print text
+
+    P = Primitive
+    primitives_fr = {
+        "ga": P(turnLeft),  "gauche": P(turnLeft),
+        "dr": P(turnRight), "droite": P(turnRight),
+        "av": P(forward),   "avance": P(forward),
+        "re": P(backward),  "recule": P(backward),
+        "vi": P(setSpeed),  "vitesse": P(setSpeed),
+        "p": P(wrap_print), "print": P(wrap_print),
+        "q": P(exit, 0),
+    }
+
+    if len(argv) > 1:
+        for script in argv[1:]:
+            retval = Evaluator(env=Env(**primitives_fr)).eval(open(script).read())
+            print " =>", retval
+    else:
+        Evaluator(env=Env(**primitives_fr)).repl()
 
--- test_logo.py
+++ test_logo.py
@@ -1,7 +1,7 @@
-from logo import Evaluator
+from logo import Evaluator, Env, Primitive
 
-def logo_eval(text):
-    return Evaluator().eval(text)
+def logo_eval(text, **env):
+    return Evaluator(Env(parent=None, **env)).eval(text)
 
 def test_eval_const_int():
     assert logo_eval("3") == 3
@@ -9,8 +9,12 @@ def test_eval_const_int():
 def test_eval_const_str():
     assert logo_eval('"This is a string"') == "This is a string"
 
+def test_eval_const_list():
+    assert logo_eval("[1 2 3]") == [1, 2, 3]
+
 def test_eval_add():
     assert logo_eval("+ 3 4") == 7
+    assert logo_eval("+ [3 4] [5 6]") == [3, 4, 5, 6]
 
 def test_eval_sub():
     assert logo_eval("- 3 4") == -1
@@ -23,3 +27,6 @@ def test_eval_div():
 
 def test_eval_proc():
     assert logo_eval("pour add1 :n + 1 :n fin add1 3") == 4
+
+def test_my_primitive():
+    assert logo_eval("f 41", f=Primitive(lambda n: n + 1)) == 42
